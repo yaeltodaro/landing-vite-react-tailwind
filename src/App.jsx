3fba84f9cf95db49d610fc1d2a50016281ef3146
Yael <y.todaro@gmail.com>
2023-03-26 10:26:49 +0200

    fix url HasRouter v3

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Iniciorick from "./components/Iniciorick";
 import Personaje from "./components/Personaje";
 import Home from "./components/Home";
@@ -6,18 +6,16 @@ import Home from "./components/Home";
 function App() {
   return (
     <div>
-  
-        <HashRouter basename="/">
-          <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/rick" element={<Iniciorick></Iniciorick>}></Route>
-            <Route
-              path="/personaje/:id"
-              element={<Personaje></Personaje>}
-            ></Route>
-          </Routes>
-        </HashRouter>
-
+      <HashRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/rick" element={<Iniciorick></Iniciorick>}></Route>
+          <Route
+            path="/personaje/:id"
+            element={<Personaje></Personaje>}
+          ></Route>
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
