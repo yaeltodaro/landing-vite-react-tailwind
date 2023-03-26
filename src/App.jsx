3857f30dc566819bce8a608edf3d82a9540c2eec
Yael <y.todaro@gmail.com>
2023-03-26 09:59:32 +0200

    fix url HasRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import Iniciorick from "./components/Iniciorick";
 import Personaje from "./components/Personaje";
-import Home from "./components/Home"
+import Home from "./components/Home";
 
 function App() {
   return (
     <div>
       <BrowserRouter basename="/landing-vite-react-tailwind/">
-        <Routes>
-          <Route exact path="/" element={ <Home/>}></Route>
-          <Route path="/rick" element={<Iniciorick></Iniciorick>}></Route>
-          <Route path="/personaje/:id" element={<Personaje></Personaje>}></Route>
-        </Routes>
+        <HashRouter>
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route path="/rick" element={<Iniciorick></Iniciorick>}></Route>
+            <Route
+              path="/personaje/:id"
+              element={<Personaje></Personaje>}
+            ></Route>
+          </Routes>
+        </HashRouter>
       </BrowserRouter>
     </div>
   );
