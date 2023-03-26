125cc8e557668032883026c97bee25bc4f07a347
Yael <y.todaro@gmail.com>
2023-03-26 12:26:41 +0200

    Ajustes css

--- src/components/Iniciorick.jsx
+++ src/components/Iniciorick.jsx
@@ -10,24 +10,41 @@ const Iniciorick = () => {
   }, []);
   return (
     <>
-    <Header></Header>
-      <h1 className="text-2xl text-center font-black p-8">Los personajes de Rick and Morty</h1>
-      <p className="text-center text-gray-800 pb-10">Datos cargados a tarvés de la API</p>
+      <Header></Header>
+      <h1 className="text-2xl text-center font-black p-8">
+        Los personajes de Rick and Morty
+      </h1>
+      <p className="text-center text-gray-800 pb-10">
+        Datos cargados a tarvés de la API
+      </p>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 place-content-center gap-8 w-full py-4 px-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 place-content-center gap-6 w-full py-4 px-8">
         {personajes != null
           ? personajes.map((personajes) => (
-              <div className="hover:bg-green-300 flex flex-col items-center" key={personajes.id}>
+              <div
+                className="bg-slate-200 rounded-xl p-4 m-3 hover:bg-green-300 flex flex-col items-center"
+                key={personajes.id}
+              >
                 <a
                   className="font-bold text-center"
                   href={`/landing-vite-react-tailwind/#/personaje/${personajes.id}`}
                 >
                   {personajes.name}
-                  <img className="rounded-full w-[200px]" src={personajes.image} alt="" />
-                  
+                  <img
+                    className="rounded-full w-[200px] mt-5 mb-5"
+                    src={personajes.image}
+                    alt=""
+                  />
                 </a>
-                <p>Especie: {personajes.species}</p>
-                <p>Location: {personajes.location.name}</p>
+
+                <p className="text-sm">Especie: {personajes.species}</p>
+                <p className="text-sm">Location: {personajes.location.name}</p>
+                <a
+                  className="text-white bg-green-600 mt-10 p-2 px-4 text-center rounded-md"
+                  href={`/landing-vite-react-tailwind/#/personaje/${personajes.id}`}
+                >
+                  Ver más ...
+                </a>
               </div>
             ))
           : "no hay personaje"}
--- src/components/Personaje.jsx
+++ src/components/Personaje.jsx
@@ -14,15 +14,15 @@ const Personaje = () => {
     <>
       <Header></Header>
       {personaje != null ? (
-        <div className="p-8 flex flex-col items-center">
+        <div className="grid justify-items-center max-w-xs bg-slate-200 rounded-xl p-5 m-0 m-auto hover:bg-green-300 ">
           <h2 className="font-black text-2xl">
             Personaje con el id {params.id}
           </h2>
           <p>Nombre: {personaje.name}</p>
-          <img className="rounded-full w-[200px]" src={personaje.image} alt="" />
+          <img className="rounded-full w-[200px] mt-10" src={personaje.image} alt="" />
 
           <a
-            className="text-gray-900 font-bold bg-orange-400 mt-4 p-2 px-4"
+            className="text-white bg-green-600 mt-10 p-2 px-4 text-center rounded-md"
             href="/landing-vite-react-tailwind/#/rick"
           >
             Atrás
